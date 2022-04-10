0954fb07213d3e1bf12fc1b77e4631e75c7f1dce
Joelle Johnson <78379264+johnsonjoelle@users.noreply.github.com>
2022-04-10 14:24:59 -0400

    Translation feature included and working

--- main.ts
+++ main.ts
@@ -1,37 +1,10 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.clearScreen()
-    basic.pause(500)
-    if (receivedNumber == 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            `)
-        music.playTone(262, music.beat(BeatFraction.Quarter))
-    } else if (receivedNumber == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # .
-            . . . . .
-            . . . . .
-            `)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-    } else {
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            . . # . .
-            . # . . .
-            # . . . .
-            `)
-    }
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    translate = true
+    basic.showString("translate on")
 })
 input.onButtonPressed(Button.A, function () {
-    msg = 0
-    radio.sendNumber(msg)
+    letter = "0"
+    radio.sendString("" + (letter))
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -40,9 +13,129 @@ input.onButtonPressed(Button.A, function () {
         . . . . .
         `)
 })
+function findLetter (text: string) {
+    if (text == letterA) {
+        basic.showString("a")
+    } else if (text == letterB) {
+        basic.showString("b")
+    } else if (text == letterC) {
+        basic.showString("c")
+    } else if (text == letterD) {
+        basic.showString("d")
+    } else if (text == letterE) {
+        basic.showString("e")
+    } else if (text == letterF) {
+        basic.showString("f")
+    } else if (text == letterG) {
+        basic.showString("g")
+    } else if (text == letterH) {
+        basic.showString("h")
+    } else if (text == letterI) {
+        basic.showString("i")
+    } else if (text == letterJ) {
+        basic.showString("j")
+    } else if (text == letterK) {
+        basic.showString("k")
+    } else if (text == letterL) {
+        basic.showString("l")
+    } else if (text == letterM) {
+        basic.showString("m")
+    } else if (text == letterN) {
+        basic.showString("n")
+    } else if (text == letterO) {
+        basic.showString("o")
+    } else if (text == letterP) {
+        basic.showString("p")
+    } else if (text == letterQ) {
+        basic.showString("q")
+    } else if (text == letterR) {
+        basic.showString("r")
+    } else if (text == letterS) {
+        basic.showString("s")
+    } else if (text == letterT) {
+        basic.showString("t")
+    } else if (text == letterU) {
+        basic.showString("u")
+    } else if (text == letterV) {
+        basic.showString("v")
+    } else if (text == letterW) {
+        basic.showString("w")
+    } else if (text == letterX) {
+        basic.showString("x")
+    } else if (text == letterY) {
+        basic.showString("y")
+    } else if (text == letterZ) {
+        basic.showString("z")
+    } else if (text == num0) {
+        basic.showString("0")
+    } else if (text == num1) {
+        basic.showString("1")
+    } else if (text == num2) {
+        basic.showString("2")
+    } else if (text == num3) {
+        basic.showString("3")
+    } else if (text == num4) {
+        basic.showString("4")
+    } else if (text == num5) {
+        basic.showString("5")
+    } else if (text == num6) {
+        basic.showString("6")
+    } else if (text == num7) {
+        basic.showString("7")
+    } else if (text == num8) {
+        basic.showString("8")
+    } else if (text == num9) {
+        basic.showString("9")
+    } else {
+        basic.showString("?")
+    }
+}
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.pause(500)
+    if (receivedString == "0") {
+        letterHolder = "" + letterHolder + receivedString
+        if (translate == false) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                `)
+        }
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else if (receivedString == "1") {
+        if (translate == false) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . # # # .
+                . . . . .
+                . . . . .
+                `)
+        } else {
+            letterHolder = "" + letterHolder + receivedString
+        }
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else {
+        if (translate == false) {
+            basic.showLeds(`
+                . . . . #
+                . . . # .
+                . . # . .
+                . # . . .
+                # . . . .
+                `)
+        } else {
+            findLetter(letterHolder)
+            letterHolder = ""
+        }
+    }
+})
 input.onButtonPressed(Button.B, function () {
-    msg = 1
-    radio.sendNumber(msg)
+    letter = "1"
+    radio.sendString("" + (letter))
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -52,7 +145,8 @@ input.onButtonPressed(Button.B, function () {
         `)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    msg = 3
+    letter = "3"
+    radio.sendString("" + (letter))
     basic.showLeds(`
         . . . . #
         . . . # .
@@ -61,11 +155,86 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         # . . . .
         `)
     basic.clearScreen()
-    radio.sendNumber(msg)
 })
-let msg = 0
+let letter = ""
+let num0 = ""
+let num9 = ""
+let num8 = ""
+let num7 = ""
+let num6 = ""
+let num5 = ""
+let num4 = ""
+let num3 = ""
+let num2 = ""
+let num1 = ""
+let letterZ = ""
+let letterY = ""
+let letterX = ""
+let letterW = ""
+let letterV = ""
+let letterU = ""
+let letterT = ""
+let letterS = ""
+let letterR = ""
+let letterQ = ""
+let letterP = ""
+let letterO = ""
+let letterN = ""
+let letterM = ""
+let letterL = ""
+let letterK = ""
+let letterJ = ""
+let letterI = ""
+let letterH = ""
+let letterG = ""
+let letterF = ""
+let letterE = ""
+let letterD = ""
+let letterC = ""
+let letterB = ""
+let letterA = ""
+let letterHolder = ""
+let translate = false
 radio.setGroup(49)
 radio.setTransmitPower(7)
+translate = false
+letterHolder = ""
+letterA = "01"
+letterB = "1000"
+letterC = "1010"
+letterD = "100"
+letterE = "0"
+letterF = "0010"
+letterG = "110"
+letterH = "0000"
+letterI = "00"
+letterJ = "0111"
+letterK = "101"
+letterL = "0100"
+letterM = "11"
+letterN = "10"
+letterO = "111"
+letterP = "0110"
+letterQ = "1101"
+letterR = "010"
+letterS = "000"
+letterT = "1"
+letterU = "001"
+letterV = "0001"
+letterW = "011"
+letterX = "1001"
+letterY = "1011"
+letterZ = "1100"
+num1 = "01111"
+num2 = "00111"
+num3 = "00011"
+num4 = "00001"
+num5 = "00000"
+num6 = "10000"
+num7 = "11000"
+num8 = "11100"
+num9 = "11110"
+num0 = "11111"
 basic.showLeds(`
     . . # . .
     . # . . .
